891e4742ef41fa621a764af79ff5a08b5ba69e62
Debak <debakroy7@gmail.com>
2023-11-05 18:39:53 +0530

    fix admin interface in pythonanywhere

--- Online_Grievance_Redressal_Portal/settings.py
+++ Online_Grievance_Redressal_Portal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$7%@0u+3b2g+4i3)r-y7&(oi8q&0__k3(dj8jt=s(wcmlemk7%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Debak.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
